da81f17acfd2d3400106bf8a85e34b17580d4376
silvioNSantini <silvio.s@live.com>
2022-07-21 11:41:28 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.alert("Direcionador de link")
+
+function n1(){
+    location.href = "http://www.google.com"
+}
+function n2(){
+    location.href = "http://www.Facebook.com"
+}
+function n3(){
+    location.href = "http://www.linkedin.com"
+}
+function n4(){
+    location.href = "http://www.twitter.com"
+}
